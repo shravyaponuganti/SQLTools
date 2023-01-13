6bb743111054ff60a6bdbeac7548dee28ede10aa
shravya <shravya.ponuganti@altusgroup.com>
2023-01-13 13:38:31 -0500

    pushed test files

---
+++ dbo/Tables/Test_123.sql
@@ -0,0 +1,4 @@
+﻿CREATE TABLE [dbo].[Test_123]
+(
+	[Id] INT NOT NULL PRIMARY KEY
+)
---
+++ dbo/Tables/Test_Shab1.sql
@@ -0,0 +1,5 @@
+﻿CREATE TABLE [dbo].[Test_Shab1] (
+    [Id] INT NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
